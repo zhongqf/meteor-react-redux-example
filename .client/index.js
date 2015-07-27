f2c9d1e5e318b037822139fd5d3bdbeb9a3b306b
zhongqf <zhongqf@gmail.com>
2015-07-28 03:59:58 +0800

    first commit

---
+++ .client/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import App from './containers/App';
+import 'todomvc-app-css/index.css';
+
+
+Template.body.onRendered(function(){
+  React.render(
+    <App />,
+    document.getElementById('root')
+  )
+});
